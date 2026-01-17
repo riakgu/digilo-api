ab3d38833a1d752821c5498650e9c2bd6abc99c8
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2026-01-17 14:03:41 +0700

    initial commit

---
+++ src/main/java/com/riakgu/digilo/DigiloApplication.java
@@ -0,0 +1,13 @@
+package com.riakgu.digilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiloApplication.class, args);
+	}
+
+}
