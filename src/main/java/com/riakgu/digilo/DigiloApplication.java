26d384feb4b7e58e307e8e8e00c8493a260b2dff
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2026-01-20 11:39:49 +0700

    feat: create base entity

--- src/main/java/com/riakgu/digilo/DigiloApplication.java
+++ src/main/java/com/riakgu/digilo/DigiloApplication.java
@@ -2,8 +2,10 @@ package com.riakgu.digilo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DigiloApplication {
 
 	public static void main(String[] args) {
