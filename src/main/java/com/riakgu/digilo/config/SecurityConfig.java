02ccb13386a53ba3d1931b85be6f1cb8f0ed85ff
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2026-01-20 19:27:02 +0700

    feat(security): add exceptionHandling in security config

--- src/main/java/com/riakgu/digilo/config/SecurityConfig.java
+++ src/main/java/com/riakgu/digilo/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.riakgu.digilo.config;
 
 import com.riakgu.digilo.security.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.time.Instant;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,6 +28,45 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json");
+
+                            String body = """
+                            {
+                              "code": "UNAUTHORIZED",
+                              "message": "Invalid or expired token",
+                              "path": "%s",
+                              "timestamp": "%s"
+                            }
+                            """.formatted(
+                                    request.getRequestURI(),
+                                    Instant.now().toString()
+                            );
+
+                            response.getWriter().write(body);
+                        })
+
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                            response.setContentType("application/json");
+
+                            String body = """
+                            {
+                              "code": "FORBIDDEN",
+                              "message": "You don't have permission",
+                              "path": "%s",
+                              "timestamp": "%s"
+                            }
+                            """.formatted(
+                                    request.getRequestURI(),
+                                    Instant.now().toString()
+                            );
+
+                            response.getWriter().write(body);
+                        })
+                )
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(
                                 "/api/auth/login",
