46a97159f228b445fe029ad6e59a23514dcf7d9a
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2026-01-21 11:00:43 +0700

    feat(exception): add AuthorizationDenied handler

--- src/main/java/com/riakgu/digilo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/riakgu/digilo/common/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,17 +20,6 @@ import java.util.Map;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-         @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<ApiResponse<Object>> handleNoResourceFound(NoResourceFoundException ex, HttpServletRequest request) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(ApiResponse.error(
-                        "RESOURCE_NOT_FOUND",
-                        "The requested resource could not be found",
-                        request.getRequestURI()
-                ));
-    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResponse<Object>> handleNotFoundException(NotFoundException ex, HttpServletRequest request) {
@@ -104,6 +94,31 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.error(
                         "BAD_REQUEST",
                         (ex.getMessage() != null) ? ex.getMessage() : "Bad request",
+                        request.getRequestURI()
+                ));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleNoResourceFound(NoResourceFoundException ex, HttpServletRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(
+                        "RESOURCE_NOT_FOUND",
+                        "The requested resource could not be found",
+                        request.getRequestURI()
+                ));
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAuthorizationDenied(
+            AuthorizationDeniedException ex,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(ApiResponse.error(
+                        "FORBIDDEN",
+                        "You don't have permission to access this resource",
                         request.getRequestURI()
                 ));
     }
